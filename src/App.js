90142f27befe019406dc1dfb00423c593ab1112e
take5150 <watanabetakehiro5150@gmail.com>
2024-02-08 21:50:09 +0900

    モーダル削除追加

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { store } from "./store";
 import { calculateTotals } from "./features/cart/CartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
       <CartContainer />
     </main>
